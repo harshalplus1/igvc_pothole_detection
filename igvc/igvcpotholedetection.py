840e76376659c8b4b05e259b3130e0575ec67be8
Harshal Kshirsagar <98384591+harshalplus1@users.noreply.github.com>
2023-02-22 18:55:28 +0530

    Add files via upload

--- igvc/igvcpotholedetection.py
+++ igvc/igvcpotholedetection.py
@@ -2,12 +2,12 @@
 import cv2 as cv
 import numpy as np
 # reading sample image
-img=cv.imread(r"/home/harshalplus1/Downloads/igvc_pothole.jpeg")
+img=cv.imread(r"/home/harshalplus1/Downloads/test1.jpeg")
 
 #taking region of interest
 def roi(img):
     #numpy array of region
-    pts=np.array([[(75,438),(290,291),(367,291),(565,438)]],dtype=np.int32)
+    pts=np.array([[(175,900),(400,400),(1100,400),(1400,900)]],dtype=np.int32)
     #making a blank image of size img
     blank=np.zeros_like(img)
     #drawing roi of white color on blank img
@@ -16,18 +16,29 @@ def roi(img):
     roii=cv.bitwise_and(roi,img)
     return roii
 
+def birdview(img):
+    w=1500
+    h=1000
+    pt1=np.float32([[(400,400),(1100,400),(175,900),(1400,900)]])
+    pt2=np.float32([[0,0],[1500,0],[350,980],[1100,980]])
+    matrix=cv.getPerspectiveTransform(pt1,pt2)
+    output=cv.warpPerspective(img,matrix,(w,h))
+    return output
+
+
 #calling function
 roii=roi(img)
+img1=birdview(roii)
 #turning gray and applying blur
-gray=cv.cvtColor(roii,cv.COLOR_BGR2GRAY)
-blur=cv.GaussianBlur(gray,(3,3),2)
+gray=cv.cvtColor(img1,cv.COLOR_BGR2GRAY)
+blur=cv.GaussianBlur(gray,(17,17),6)
 #turning the image into black and white min and max parameters
-(thresh, BnW_image) = cv.threshold(blur, 200, 255, cv.THRESH_BINARY)
+(thresh, BnW_image) = cv.threshold(blur, 170, 200, cv.THRESH_BINARY)
 #canny edge detection
 canny=cv.Canny(BnW_image,125,255)
 #finding contour lines on canny image and drawing it on final image
 contours, hierarchies=cv.findContours(canny,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
-finalimg=cv.drawContours(img,contours,-1,(0,0,0),3)
-cv.imshow("new",finalimg)
+finalimg=cv.drawContours(img1,contours,-1,(0,0,0),6)
+cv.imshow("new",img1)
 cv.waitKey(0)
 cv.destroyAllWindows()
